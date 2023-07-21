8d44ce2e0ae7aac156d76a0b8ab8ad5171dfe5ac
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2023-07-21 10:25:43 +0000

    Add copyright, license, include guards

--- include/test_jtc_trajectories/sample_trajectory.hpp
+++ include/test_jtc_trajectories/sample_trajectory.hpp
@@ -1,3 +1,23 @@
+// Copyright 2023 Austrian Institute of Technology
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TEST_JTC_TRAJECTORIES__SAMPLE_TRAJECTORY_HPP_
+#define TEST_JTC_TRAJECTORIES__SAMPLE_TRAJECTORY_HPP_
+
+#include <memory>
+#include <vector>
+
 #include "joint_trajectory_controller/interpolation_methods.hpp"
 #include "joint_trajectory_controller/trajectory.hpp"
 #include "trajectory_msgs/msg/joint_trajectory.hpp"
@@ -156,3 +176,5 @@ private:
   bool has_velocity_ = false;
   bool has_acceleration_ = false;
 };
+
+#endif  // TEST_JTC_TRAJECTORIES__SAMPLE_TRAJECTORY_HPP_
